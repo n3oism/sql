bf5195068b30d6c5cbb9b7a43b6ddd08c1ef9937
Hyunsu, Yoo <n3oism@gmail.com>
2018-01-13 13:06:04 +0900

    Update mssql_id.php

--- examples/chap3/mssql_id.php
+++ examples/chap3/mssql_id.php
@@ -10,7 +10,12 @@ if (!mssql_select_db('northwind', $link)) {
     exit;
 }
 
-$_empid  = $_GET['empid'];
+if(!isset($_GET['empid'])) {
+ $_empid=1;
+} else {
+ $_empid  = $_GET['empid'];
+}
+
 $sql    = 'SELECT * FROM Employees WHERE EmployeeID='. $_empid;
 $result = mssql_query($sql, $link);
 
@@ -20,11 +25,13 @@ if (!$result) {
     exit;
 }
 
-print "<table border=1 cellpadding=5 cellspacing=0>\n";
-print "\t<tr>\n\t\t<td>Employee ID</td><td>First Name</td><td>Title</td><td>Hire Date</td>\n\t</tr>";
-
 $row = mssql_fetch_assoc($result);
-print "\t<tr>\n\t\t<td>" . $row['EmployeeID'] . "</td><td>" . $row['FirstName'] . "</td><td>" . $row['Title'] . "</td><td>" . $row['HireDate'] . "</td>\n\t</tr>\n";
+print "<b>직원정보</b><br><br>";
+print "<table border=1 cellpadding=5 cellspacing=0>\n";
+print "\t<tr>\n\t\t<td>직원번호</td><td>" . str_pad($row['EmployeeID'],8,"0",STR_PAD_LEFT) . "</td>\n";
+print "\t<tr>\n\t\t<td>이름</td><td>" . $row['FirstName'] . "</td></tr>\n";
+print "\t<tr>\n\t\t<td>직급</td><td>" . $row['Title'] . "</td>\n";
+print "\t<tR>\n\t\t<td>입사일자</td><td>" . $row['HireDate'] . "</td>\n\t</tr>\n";
 print "</table>\n";
 
 mssql_close($link);
